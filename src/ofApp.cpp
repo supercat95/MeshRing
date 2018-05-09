1babb3181b16c8f65c4c735c89517d964fd41955
supercat95 <joybates@wrls10-8-5-3.resnet.smu.edu>
2018-05-08 20:42:05 -0500

    Save

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,74 @@
 #include "ofApp.h"
+#include "ofMesh.h"
+
+int xPos = 1, yPos = 0, yPos2 = 60, zPos = 1, theta = 10, loop = 36, radius = 300;
+ofVec3f v1, v2, v3;
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-
+ofApp::~ofApp()
+{
+    ring.clear();
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::setup()
+{
+    
+    for (int i = 0; i < loop; i++)
+    {
+        v1.set(xPos, yPos, zPos); v2.set(xPos + 30, yPos2, zPos); v3.set(xPos + 60, yPos, zPos); // left, up, down
 
+//        vectors.push_back(v1); vectors.push_back(v2); vectors.push_back(v3);
+
+        ring.addVertex(v1); ring.addVertex(v2); ring.addVertex(v3);
+        ring.setupIndicesAuto();
+
+        ofEnableDepthTest();
+
+        xPos += 30;
+        zPos += 30;
+    }
+    
+    
+    // sets x, y, and z coordinates of points
+//    for (int i = 0; i < loop; i++)
+//    {
+////        xPos*=-1;
+//        yPos2*=-1;
+////        zPos*=-1;
+//
+//        p.set(xPos, yPos2, zPos);
+//
+//        p.normalize(); // centers points into a ring
+//        p *= radius;
+//        points.push_back(p);
+//
+//        vectors.push_back(p);
+//
+//        ring.addVertex(vectors[i]); ring.addVertex(vectors[i+1]); ring.addVertex(vectors[i+2]);
+//
+//        ofEnableDepthTest(); // Adds depth to look more 3D
+//    }
+    
+//    for (int i = 0; i < loop; i++)
+//    {
+//        degree.push_back(ofRandom(0,10)), x.push_back(ofRandom(0,10)), y.push_back(ofRandom(0,10)), z.push_back(ofRandom(0,10));
+//    }
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::update()
+{
+    theta += 1;
+}
 
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    ofRotateY(theta);
+    ring.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    ring.draw();
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,10 +1,25 @@
 #pragma once
 
 #include "ofMain.h"
+#include <list>
+#include <vector>
+
+using std::list;
+using std::vector;
 
 class ofApp : public ofBaseApp{
-
+    
+    ofMesh ring;
+    
+    ofPoint p, p1, p2, p3;
+    list<ofPoint> points;
+    vector<ofVec3f> vectors;
+    
+//    vector<int> r, g, b, degree, x, y, z;
+    
 	public:
+        ~ofApp();
+    
 		void setup();
 		void update();
 		void draw();
